fe9f524a0b07c41b1f067dea416c53ee18f8cef2
jypark <jypark@da-rum.com>
2024-11-06 17:10:51 +0900

    Initial commit

---
+++ src/test/java/com/jypark/playlistweb/PlaylistWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jypark.playlistweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
