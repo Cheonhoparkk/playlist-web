4d815ea870ba56dd296fdba5743445a2bc45d5c2
Cheonhoparkk <a20489506@gmail.com>
2024-12-07 13:02:51 +0900

    시큐리티 main-view 허가 수정

--- src/main/java/com/jypark/playlistweb/config/SecurityConfig.java
+++ src/main/java/com/jypark/playlistweb/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 // 루트 페이지, 로그인 페이지, 그리고 css와 js파일에 대해 인증 없이 접근 가능( + img, lib)
-                                .requestMatchers("/", "/login/login-view", "/css/**", "/js/**", "/img/**", "/lib/**").permitAll()
+                                .requestMatchers("/", "/css/**", "/js/**", "/img/**", "/lib/**").permitAll()
                                 // 그 외 모든 요청은 인증이 필요
                                 .anyRequest().authenticated()
                 )
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .oauth2Login(oauth2Login ->
                         oauth2Login
                                 // 사용자 정의 로그인 페이지 경로 설정
-                                .loginPage("/login/login-view")
+                                .loginPage("/login/login-view").permitAll()
                                 // 로그인 성공 시 리디렉션할 경로 설정
                                 .defaultSuccessUrl("/main/main-view")
                 )
